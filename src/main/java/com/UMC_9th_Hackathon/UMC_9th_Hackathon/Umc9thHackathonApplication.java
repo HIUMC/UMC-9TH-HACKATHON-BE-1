b4f6896e134e60dafa1acf62d8ad5df3923240af
ccjngwn <ahaha620@gmail.com>
2026-01-08 10:45:56 +0900

    feat: initial commit

---
+++ src/main/java/com/UMC_9th_Hackathon/UMC_9th_Hackathon/Umc9thHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.UMC_9th_Hackathon.UMC_9th_Hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc9thHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc9thHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UMC_9th_Hackathon/UMC_9th_Hackathon/Umc9thHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UMC_9th_Hackathon.UMC_9th_Hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc9thHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
